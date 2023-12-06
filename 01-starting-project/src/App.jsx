e4d7f39e8dc113829ebf4a86a8a4fb6e2eb259db
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-12-06 13:24:44 +0200

    refactoring components for react essentials app

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,76 +1,17 @@
-import { useState } from "react";
-import { CORE_CONCEPTS } from "./data-with-examples.js";
-import { EXAMPLES } from "./data-with-examples.js";
 import Header from "../src/components/Header/Header.jsx";
-import CoreConcept from "../src/components/CoreConcept.jsx";
-import TabButton from "../src/components/TabButton.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [selectedContent, setSelectedContent] = useState("");
-  function handleClick(clickedEl) {
-    setSelectedContent(clickedEl);
-  }
+  
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem, index) => {
-              return <CoreConcept key={index} {...conceptItem} />;
-            })}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedContent === "components"}
-              handleClick={() => {
-                handleClick("components");
-              }}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedContent === "jsx"}
-              handleClick={() => {
-                handleClick("jsx");
-              }}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedContent === "props"}
-              handleClick={() => {
-                handleClick("props");
-              }}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedContent === "state"}
-              handleClick={() => {
-                handleClick("state");
-              }}
-            >
-              State
-            </TabButton>
-          </menu>
-          {!selectedContent && <p>Please select a topic!</p>}
-          {selectedContent && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedContent].title}</h3>
-              <p>{EXAMPLES[selectedContent].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedContent].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,9 @@
-export default function TabButton({ children, handleClick, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={handleClick}>{children}</button>
+      <button className={isSelected ? "active" : undefined} {...props}>
+        {children}
+      </button>
     </li>
   );
 }
