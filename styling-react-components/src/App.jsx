594d3b8055a52e827d2df43a8f640e5603987e8a
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-12-14 04:18:39 +0200

    clear code for lecture challenges

--- styling-react-components/src/App.jsx
+++ styling-react-components/src/App.jsx
@@ -11,30 +11,3 @@ export default function App() {
     </>
   );
 }
-
-
-// import { useState } from "react";
-// function App() {
-//   const [choice, setChoice] = useState(null)
-//   let textColor = '';
-//   if (choice === 'yes') {
-//     textColor = "highlight-green"
-//   }else if (choice === 'no') {
-//     textColor = "highlight-red"
-//   }
-//   return (
-//     <div id="app">
-//       <h1 className={`${textColor}`}>CSS is great!</h1>
-//       <menu>
-//         <li>
-//           <button onClick={() => setChoice('yes')}>Yes</button>
-//         </li>
-//         <li>
-//           <button onClick={() => setChoice('no')}>No</button>
-//         </li>
-//       </menu>
-//     </div>
-//   );
-// }
-
-// export default App;
