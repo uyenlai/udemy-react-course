c84c4602c72a84e302ab2ebe99b906494f2788dc
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-12-21 23:25:04 +0200

    lectures finished

---
+++ side-effects-and-useEffect-hook/src/components/Modal.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+function Modal({ children, open, onClose }) {
+  const dialog = useRef();
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog className="modal" onClose={onClose} ref={dialog}>
+      {open ? children : null}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
+
+export default Modal;
