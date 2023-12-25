5e24c93984e1047c2acdf324b5229f26a7b3bf78
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-12-25 23:24:10 +0200

    sending http Get request via useEffect

---
+++ sending-http-requests/src/components/AvailablePlaces.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
