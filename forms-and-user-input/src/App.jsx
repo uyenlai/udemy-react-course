d481e60eb4f39d6dc8e91baf68795aee6d92dc00
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-12-26 23:11:06 +0200

    getting values via FormData

--- forms-and-user-input/src/App.jsx
+++ forms-and-user-input/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
 import StateLogin from './components/StateLogin.jsx';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
       <Header />
       <main>
         {/* <Login /> */}
-        <StateLogin />
+        {/* <StateLogin /> */}
+        <Signup />
       </main>
     </>
   );
