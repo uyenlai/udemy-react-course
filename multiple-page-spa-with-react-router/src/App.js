722d6e4034916bd8776d4234c21ae6b7b7182033
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-04 00:44:54 +0200

    defining routes with createBrowserRouter and RouterProvider

---
+++ multiple-page-spa-with-react-router/src/App.js
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+function App() {
+  const router = createBrowserRouter([{ path: "/", element: <Home /> }, {}]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
