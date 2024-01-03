15e628ffc6b323b38ee53450439f5416cadfa63b
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-04 01:02:48 +0200

    define routes with the help of createRoutesFromElements function and Route component

--- multiple-page-spa-with-react-router/src/App.js
+++ multiple-page-spa-with-react-router/src/App.js
@@ -1,11 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/products", element: <Products /> },
-  ]);
+  const routeDefinitions = createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+    </Route>
+  );
+
+  const router = createBrowserRouter(routeDefinitions);
+
+  // const router = createBrowserRouter([
+  //   { path: "/", element: <Home /> },
+  //   { path: "/products", element: <Products /> },
+  // ]);
   return <RouterProvider router={router} />;
 }
 
