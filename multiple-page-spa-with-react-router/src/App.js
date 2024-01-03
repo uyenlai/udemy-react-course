a293502e522fb4416e2a40d3c902cb9096729123
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-04 01:11:40 +0200

    navigate between pages with Link component

--- multiple-page-spa-with-react-router/src/App.js
+++ multiple-page-spa-with-react-router/src/App.js
@@ -1,25 +1,24 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route,
+  //createRoutesFromElements,
+  //Route,
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 function App() {
-  const routeDefinitions = createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Home />} />
-      <Route path="/products" element={<Products />} />
-    </Route>
-  );
+  // const routeDefinitions = createRoutesFromElements(
+  //   <Route>
+  //     <Route path="/" element={<Home />} />
+  //     <Route path="/products" element={<Products />} />
+  //   </Route>
+  // );
+  //const router = createBrowserRouter(routeDefinitions);
 
-  const router = createBrowserRouter(routeDefinitions);
-
-  // const router = createBrowserRouter([
-  //   { path: "/", element: <Home /> },
-  //   { path: "/products", element: <Products /> },
-  // ]);
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/products", element: <Products /> },
+  ]);
   return <RouterProvider router={router} />;
 }
 
