159a6c4530d0882bb9f729a67f84daaf263dee64
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-04 03:13:29 +0200

    show ErrorPage with errorElement

--- multiple-page-spa-with-react-router/src/App.js
+++ multiple-page-spa-with-react-router/src/App.js
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
+      errorElement: <ErrorPage />,
       element: <RootLayout />,
       children: [
         { path: "/", element: <Home /> },
