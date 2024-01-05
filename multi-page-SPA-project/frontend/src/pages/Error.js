42ff8d647deb4de56b6c6f06b04b7c8a9316a7d0
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-05 15:33:20 +0200

    error handling

--- multi-page-SPA-project/frontend/src/pages/Error.js
+++ multi-page-SPA-project/frontend/src/pages/Error.js
@@ -1,7 +1,26 @@
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+
 export default function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occured!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data.message);
+  }
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
   return (
     <>
-      <h1>An error occured!</h1>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </>
   );
 }
--- multi-page-SPA-project/frontend/src/pages/Events.js
+++ multi-page-SPA-project/frontend/src/pages/Events.js
@@ -2,7 +2,11 @@ import EventsList from "../components/EventsList";
 import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  // if (data.isTrue) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
   return <EventsList events={events} />;
 }
 export default EventsPage;
@@ -11,8 +15,11 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
+    //return { isError: true, message: "Could not fetch events!" };
+    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+      status: 201,
+    });
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
