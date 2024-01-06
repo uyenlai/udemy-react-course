cfdfa8af999664b82059bdfe3f271c59d706e766
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-06 02:39:11 +0200

    dynamic routes and loader()s

--- multi-page-SPA-project/frontend/src/pages/EventDetail.js
+++ multi-page-SPA-project/frontend/src/pages/EventDetail.js
@@ -1,12 +1,23 @@
-import { useParams, Link } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  const params = useParams();
-  return (
-    <>
-      <h1>Event Detail Page</h1>
-      <p>{params.eventId}</p>
-      <Link to="/events">back</Link>
-    </>
-  );
+  const data = useLoaderData();
+  return <EventItem event={data.event} />;
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch the selected event." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
 }
