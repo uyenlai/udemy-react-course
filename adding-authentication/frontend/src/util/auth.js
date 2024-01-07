03a241b24bf740dad973fc77df550151d3ec0d48
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-01-07 16:12:02 +0200

    add automatic logout and manage the token expiration

--- adding-authentication/frontend/src/util/auth.js
+++ adding-authentication/frontend/src/util/auth.js
@@ -2,6 +2,17 @@ import { redirect } from "react-router-dom";
 
 export function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 }
 
@@ -17,4 +28,12 @@ export function chechAuthLoader() {
     return redirect("/authentication");
   }
   return null;
+}
+
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
 }
