cb5d7e0f158297ec80b07968ca7ca4ce5aa3cad3
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-02-04 18:21:52 +0200

    type inference, union, and aliases

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -36,3 +36,30 @@ people = [
   { name: "John", age: 55 },
   { name: "Renee", age: 25 },
 ];
+
+//Type inference + union
+let course: string | number = "React course";
+
+course = 12345;
+
+//Type Aliases
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+let product: Product;
+
+product = {
+  id: 1,
+  name: "Hat",
+  price: 20,
+};
+
+let products: Product[];
+
+products = [
+  { id: 1, name: "Glasses", price: 15 },
+  { id: 2, name: "T-Shirt", price: 65 },
+];
